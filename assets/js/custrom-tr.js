971482dd09fb0365f79d3687a0bf9e804c8696f7
luutrang308 <luutrangk10@gmail.com>
2021-05-11 17:10:29 +0700

    picon

--- assets/js/custrom-tr.js
+++ assets/js/custrom-tr.js
@@ -1,29 +1,37 @@
 $(document).ready(function(){
   new WOW().init();
-  
-  $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-      $('.section_header').addClass('active');
-    } else {
-      $('.section_header').removeClass('active');
-    }
-  });
+  /* site35_head_col0_icon */
+  $('.open_form').click(function() {
+    $('.form_search').toggleClass('active')
+  })
 
-  $('.form_item img').click(function() {
-    $('.form_item').toggleClass('active');
-  });
-  $('.form_item p').click(function() {
-    $('.form_item').toggleClass('active');
-  });
+  $('.icon_open_menu').click(function() {
+    $('.section_header').toggleClass('active')
+  })
 
   $('.slider_banner').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    autoplaySpeed: 1200,
+    autoplaySpeed: 6000,
+    arrows: false,
     slidesToShow: 1,
     adaptiveHeight: true,
     autoplay: true,
+  });
+
+  $('.slider_categories').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    autoplaySpeed: 8000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,    
+    arrows: true,
+    prevArrow: "<button class='prev slick-prev'><img class='left-arrow ' src='./assets/img/left.png' alt=''></button>",
+    nextArrow: "<button class='next slick-next'><img class='right-arrow ' src='./assets/img/right.png' alt=''></button>",
+    
   });
 
   $(window).scroll(function(){
@@ -37,6 +45,41 @@ $(document).ready(function(){
     $('html, body').animate({scrollTop : 0},800);
     return false;
   });
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.site35_head_col0_icon').addClass('active');
+    } else {
+      $('.site35_head_col0_icon').removeClass('active');
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+  $('.form_item img').click(function() {
+    $('.form_item').toggleClass('active');
+  });
+  $('.form_item p').click(function() {
+    $('.form_item').toggleClass('active');
+  });
+
+  
+
+  
 
 
 });
