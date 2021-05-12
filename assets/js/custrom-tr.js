7c406573c8386c3eba064101a36f48ec5d33fb58
luutrang308 <luutrangk10@gmail.com>
2021-05-12 17:22:56 +0700

    picon

--- assets/js/custrom-tr.js
+++ assets/js/custrom-tr.js
@@ -34,6 +34,74 @@ $(document).ready(function(){
     
   });
 
+
+  $('.slide_for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slide_nav'
+  });
+  $('.slide_nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slide_for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    arrows: true,
+    prevArrow: "<button class='prev slick-prev'><img class='left-arrow ' src='./assets/img/left.png' alt=''></button>",
+    nextArrow: "<button class='next slick-next'><img class='right-arrow ' src='./assets/img/right.png' alt=''></button>",
+    
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.slider_lichsu').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    autoplaySpeed: 6000,
+    arrows: false,
+    slidesToShow: 4,
+    adaptiveHeight: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+
+
+
   $(window).scroll(function(){
     if ($(this).scrollTop() > 500) {
       $('.backtotop').fadeIn();
